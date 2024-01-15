c5dae21112e7a2fb8e11d4821aa0e84c26d95ab7
Arjuna Dasanayakage <arjunahiran@gmail.com>
2024-01-15 17:48:34 +0000

    Add auto scroll functionality

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { ChatMessage } from "@/components/chat-message";
 
 const apiUrl = "http://127.0.0.1:5000/api/chat";
@@ -7,6 +7,17 @@ const apiUrl = "http://127.0.0.1:5000/api/chat";
 export default function Home() {
   const [chats, setChats] = useState([]);
   const [question, setQuestion] = useState("");
+
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (chats.length) {
+      ref.current?.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+      });
+    }
+  }, [chats.length]);
 
   const handleSendQuestion = async () => {
     if (!question.trim()) {
@@ -57,57 +68,9 @@ export default function Home() {
     );
   }
 
-  function ChatBox() {
-    return (
-      <div className="fixed inset-x-0 bottom-0 w-full bg-gradient-to-b from-muted/30 from-0% to-muted/30 to-50% animate-in duration-300 ease-in-out dark:from-background/10 dark:from-10% dark:to-background/80 peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px]">
-        <div className="mx-auto sm:max-w-2xl sm:px-4">
-          <div className="flex h-12 items-center justify-center"></div>
-          <div className="space-y-4 border-t bg-background px-4 py-2 shadow-lg sm:rounded-t-xl sm:border md:py-4">
-            <div className="relative flex max-h-60 w-full grow flex-col overflow-hidden bg-background px-2 sm:rounded-md sm:border sm:px-2">
-              <textarea
-                name="message"
-                tabIndex={0}
-                rows={1}
-                value={question}
-                onChange={(e) => setQuestion(e.target.value)}
-                placeholder="Send a message."
-                spellCheck={true}
-                className="min-h-[60px] w-full resize-none bg-transparent px-4 py-[1.3rem] focus-within:outline-none sm:text-sm"
-              />
-              <div className="absolute right-0 top-4 sm:right-4">
-                <button
-                  className="inline-flex items-center justify-center rounded-full text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow-md hover:bg-primary/90 h-8 w-8 p-0"
-                  type="submit"
-                  data-state="closed"
-                  onClick={handleSendQuestion}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 256 256"
-                    fill="currentColor"
-                    className="h-4 w-4"
-                  >
-                    <path d="M200 32v144a8 8 0 0 1-8 8H67.31l34.35 34.34a8 8 0 0 1-11.32 11.32l-48-48a8 8 0 0 1 0-11.32l48-48a8 8 0 0 1 11.32 11.32L67.31 168H184V32a8 8 0 0 1 16 0Z"></path>
-                  </svg>
-                  <span className="sr-only">Send message</span>
-                </button>
-              </div>
-            </div>
-            <p className="px-2 text-center text-xs leading-normal text-muted-foreground hidden sm:block">
-              &copy; {new Date().getFullYear()} Smart AI Tutor - Your Virtual
-              Learning Companion. Built for{" "}
-              <span className="font-semibold">Introduction to AI - COM727</span>
-              .
-            </p>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
   function Welcome() {
     return (
-      <div className="pb-[200px] pt-4 md:pt-10">
+      <div className="pt-4 md:pt-10">
         <div className="mx-auto max-w-2xl px-4">
           <div className="rounded-lg border bg-background p-8">
             <h1 className="mb-2 text-lg font-semibold">
@@ -136,17 +99,20 @@ export default function Home() {
   return (
     <main className="flex flex-1 flex-col">
       <div className="relative flex min-h-full overflow-hidden">
-        <div className="group w-full overflow-auto pl-0 animate-in duration-300 ease-in-out peer-[[data-state=open]]:lg:pl-[250px] peer-[[data-state=open]]:xl:pl-[300px]">
-          {chats.length ? (
-            chats.map((chat, index) => (
-              <div key={index}>
-                <ChatMessage message={chat} />
-                {index < chats.length - 1 && <div className="my-4 md:my-8" />}
-              </div>
-            ))
-          ) : (
-            <Welcome />
-          )}
+        <div className="w-full overflow-auto pl-0">
+          <div className="pb-[200px]">
+            {chats.length ? (
+              chats.map((chat, index) => (
+                <div key={index}>
+                  <ChatMessage message={chat} />
+                  {index < chats.length - 1 && <div className="my-4 md:my-8" />}
+                </div>
+              ))
+            ) : (
+              <Welcome />
+            )}
+          </div>
+          <div ref={ref}></div>
 
           {/** chat box */}
           <div className="fixed inset-x-0 bottom-0 w-full bg-gradient-to-b from-muted/30 from-0% to-muted/30 to-50% animate-in duration-300 ease-in-out dark:from-background/10 dark:from-10% dark:to-background/80 peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px]">
@@ -160,6 +126,13 @@ export default function Home() {
                     rows={1}
                     value={question}
                     onChange={(e) => setQuestion(e.target.value)}
+                    onKeyDown={(e) => {
+                      if (e.keyCode == 13 && e.shiftKey == false) {
+                        e.preventDefault();
+                        handleSendQuestion();
+                        return false;
+                      }
+                    }}
                     placeholder="Send a message."
                     spellCheck={true}
                     className="min-h-[60px] w-full resize-none bg-transparent px-4 py-[1.3rem] focus-within:outline-none"
--- frontend/src/components/chat-message.js
+++ frontend/src/components/chat-message.js
@@ -22,15 +22,16 @@ const Typewriter = ({ text, delay }) => {
 };
 
 export default Typewriter;
-export function ChatMessage({ message }) {
+export const ChatMessage = ({ message }) => {
   function UserIcon() {
     return (
-      <div className="flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border shadow bg-background">
+      <div className="flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border shadow bg-background text-gray-500">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 256 256"
           fill="currentColor"
           className="h-4 w-4"
+          strokeWidth={1.5}
         >
           <path d="M230.92 212c-15.23-26.33-38.7-45.21-66.09-54.16a72 72 0 1 0-73.66 0c-27.39 8.94-50.86 27.82-66.09 54.16a8 8 0 1 0 13.85 8c18.84-32.56 52.14-52 89.07-52s70.23 19.44 89.07 52a8 8 0 1 0 13.85-8ZM72 96a56 56 0 1 1 56 56 56.06 56.06 0 0 1-56-56Z"></path>
         </svg>
@@ -71,4 +72,4 @@ export function ChatMessage({ message }) {
       </div>
     </div>
   );
-}
+};
